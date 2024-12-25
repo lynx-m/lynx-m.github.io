ff4df4e21243066e65f860b96da36ed653bcdea8
LM <151560704+lynx-m@users.noreply.github.com>
2024-12-25 18:58:44 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -266,11 +266,11 @@
 			for (i = 0; i < coll.length; i++) {
 			  coll[i].addEventListener("click", function() {
 				this.classList.toggle("active");
-				var content = this.nextElementSibling;
-				if (content.style.display === "block") {
-				  content.style.display = "none";
+				var collcontent = this.nextElementSibling;
+				if (collcontent.style.display === "block") {
+				  collcontent.style.display = "none";
 				} else {
-				  content.style.display = "block";
+				  collcontent.style.display = "block";
 				}
 			  });
 			}
