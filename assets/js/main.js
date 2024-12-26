2ed46542889f904a07df4e0b1bd6ac9468fdced8
LM <151560704+lynx-m@users.noreply.github.com>
2024-12-25 19:14:59 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -267,11 +267,11 @@
 			for (i = 0; i < coll.length; i++) {
 			  coll[i].addEventListener("click", function() {
 				this.classList.toggle("active");
-				var collcontent = this.nextElementSibling;
-				if (collcontent.style.display === "block") {
-				  collcontent.style.display = "none";
+				var content = this.nextElementSibling;
+				if (content.style.display === "block") {
+				  content.style.display = "none";
 				} else {
-				  collcontent.style.display = "block";
+				  content.style.display = "block";
 				}
 			  });
 			}
