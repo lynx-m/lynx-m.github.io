ab0043783a5c23db25d971e8cb0fe203b97ffeed
LM <151560704+lynx-m@users.noreply.github.com>
2024-12-27 16:43:42 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -277,22 +277,33 @@
 
 	// Slider
 
-			var slideIndex = 1;
-			showDivs(slideIndex);
-		
-			function plusDivs(n) {
-			  showDivs(slideIndex += n);
+			let slideIndex = 1;
+			showSlides(slideIndex);
+			
+			// Next/previous controls
+			function plusSlides(n) {
+			  showSlides(slideIndex += n);
 			}
 			
-			function showDivs(n) {
-			  var i;
-			  var x = document.getElementsByClassName("mySlides");
-			  if (n > x.length) {slideIndex = 1}
-			  if (n < 1) {slideIndex = x.length} ;
-			  for (i = 0; i < x.length; i++) {
-			    x[i].style.display = "none";
+			// Thumbnail image controls
+			function currentSlide(n) {
+			  showSlides(slideIndex = n);
+			}
+			
+			function showSlides(n) {
+			  let i;
+			  let slides = document.getElementsByClassName("mySlides");
+			  let dots = document.getElementsByClassName("dot");
+			  if (n > slides.length) {slideIndex = 1}
+			  if (n < 1) {slideIndex = slides.length}
+			  for (i = 0; i < slides.length; i++) {
+			    slides[i].style.display = "none";
 			  }
-			  x[slideIndex-1].style.display = "block";
+			  for (i = 0; i < dots.length; i++) {
+			    dots[i].className = dots[i].className.replace(" active", "");
+			  }
+			  slides[slideIndex-1].style.display = "block";
+			  dots[slideIndex-1].className += " active";
 			}
 
 })(jQuery);
