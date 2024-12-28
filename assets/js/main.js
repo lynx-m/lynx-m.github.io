d83e68768bf8dce6bbb6a6a48bb38a427c53d04f
LM <151560704+lynx-m@users.noreply.github.com>
2024-12-28 16:08:32 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -293,6 +293,8 @@
 			  x[slideIndex-1].style.display = "block";
 			}
 
-			button.addEventListener("click", plusDivs(+1));
+			var but = document.getElementsByClassName("onclick")
+			for (i = 0; i < but.length; i++) {
+			  but[i].addEventListener("click", plusDivs())}
 
 })(jQuery);
