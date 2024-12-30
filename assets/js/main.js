99264034e74cd46a2b4171fc3873d785a19535d7
LM <151560704+lynx-m@users.noreply.github.com>
2024-12-30 18:22:59 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -275,12 +275,11 @@
 			  });
 			}
 
-			var slideIndex = 2;
+			var slideIndex = 1;
 			showDivs(slideIndex);
 			
 			function plusDivs(n) {
-			  slideIndex += n;
-			  showDivs(slideIndex);
+			  showDivs(slideIndex += n);
 			}
 			
 			function showDivs(n) {
@@ -291,7 +290,7 @@
 			  for (i = 0; i < x.length; i++) {
 			    x[i].style.display = "none";
 			  }
-			  x[n-1].style.display = "block";
+			  x[slideIndex-1].style.display = "block";
 			}
 
 })(jQuery);
